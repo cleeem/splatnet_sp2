d0394af66e944b8b1ea40de66fc77efd4249a685
cleeem <sp2.clem@gmail.com>
2022-09-03 13:41:47 +0200

    changements

---
+++ iksm.py
@@ -0,0 +1,328 @@
+# eli fessler
+# clovervidia
+from __future__ import print_function
+from builtins import input
+import requests, json, re, sys
+import os, base64, hashlib
+import uuid, time, random, string
+from bs4 import BeautifulSoup
+
+session = requests.Session()
+version = "unknown"
+nsoapp_version = "2.2.0"
+
+# structure:
+# log_in() -> get_session_token()
+# get_cookie() -> call_imink_api() -> f
+# enter_cookie()
+# get_nsoapp_version()
+
+# place config.txt in same directory as script (bundled or not)
+if getattr(sys, 'frozen', False):
+	app_path = os.path.dirname(sys.executable)
+elif __file__:
+	app_path = os.path.dirname(__file__)
+config_path = os.path.join(app_path, "config.txt")
+
+def get_nsoapp_version():
+	'''Fetches the current Nintendo Switch Online app version from the Apple App Store.'''
+
+	global nsoapp_version
+	try:
+		page = requests.get("https://apps.apple.com/us/app/nintendo-switch-online/id1234806557")
+		soup = BeautifulSoup(page.text, 'html.parser')
+		elt = soup.find("p", {"class": "whats-new__latest__version"})
+		ver = elt.get_text().replace("Version ","").strip()
+		return ver
+	except:
+		return nsoapp_version
+
+def log_in(ver):
+	'''Logs in to a Nintendo Account and returns a session_token.'''
+
+	global version
+	version = ver
+
+	auth_state = base64.urlsafe_b64encode(os.urandom(36))
+
+	auth_code_verifier = base64.urlsafe_b64encode(os.urandom(32))
+	auth_cv_hash = hashlib.sha256()
+	auth_cv_hash.update(auth_code_verifier.replace(b"=", b""))
+	auth_code_challenge = base64.urlsafe_b64encode(auth_cv_hash.digest())
+
+	app_head = {
+		'Host':                      'accounts.nintendo.com',
+		'Connection':                'keep-alive',
+		'Cache-Control':             'max-age=0',
+		'Upgrade-Insecure-Requests': '1',
+		'User-Agent':                'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Mobile Safari/537.36',
+		'Accept':                    'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8n',
+		'DNT':                       '1',
+		'Accept-Encoding':           'gzip,deflate,br',
+	}
+
+	body = {
+		'state':                               auth_state,
+		'redirect_uri':                        'npf71b963c1b7b6d119://auth',
+		'client_id':                           '71b963c1b7b6d119',
+		'scope':                               'openid user user.birthday user.mii user.screenName',
+		'response_type':                       'session_token_code',
+		'session_token_code_challenge':        auth_code_challenge.replace(b"=", b""),
+		'session_token_code_challenge_method': 'S256',
+		'theme':                               'login_form'
+	}
+
+	url = 'https://accounts.nintendo.com/connect/1.0.0/authorize'
+	r = session.get(url, headers=app_head, params=body)
+
+	post_login = r.history[0].url
+
+	print("\nMake sure you have fully read the \"Cookie generation\" section of the readme before proceeding. To manually input a cookie instead, enter \"skip\" at the prompt below.")
+	print("\nNavigate to this URL in your browser:")
+	print(post_login)
+	print("Log in, right click the \"Select this account\" button, copy the link address, and paste it below:")
+	while True:
+		try:
+			use_account_url = input("")
+			if use_account_url == "skip":
+				return "skip"
+			session_token_code = re.search('de=(.*)&', use_account_url)
+			return get_session_token(session_token_code.group(1), auth_code_verifier)
+		except KeyboardInterrupt:
+			print("\nBye!")
+			sys.exit(1)
+		except AttributeError:
+			print("Malformed URL. Please try again, or press Ctrl+C to exit.")
+			print("URL:", end=' ')
+		except KeyError: # session_token not found
+			print("\nThe URL has expired. Please log out and back into your Nintendo Account and try again.")
+			sys.exit(1)
+
+def get_session_token(session_token_code, auth_code_verifier):
+	'''Helper function for log_in().'''
+
+	nsoapp_version = get_nsoapp_version()
+
+	app_head = {
+		'User-Agent':      'OnlineLounge/' + nsoapp_version + ' NASDKAPI Android',
+		'Accept-Language': 'en-US',
+		'Accept':          'application/json',
+		'Content-Type':    'application/x-www-form-urlencoded',
+		'Content-Length':  '540',
+		'Host':            'accounts.nintendo.com',
+		'Connection':      'Keep-Alive',
+		'Accept-Encoding': 'gzip'
+	}
+
+	body = {
+		'client_id':                   '71b963c1b7b6d119',
+		'session_token_code':          session_token_code,
+		'session_token_code_verifier': auth_code_verifier.replace(b"=", b"")
+	}
+
+	url = 'https://accounts.nintendo.com/connect/1.0.0/api/session_token'
+
+	r = session.post(url, headers=app_head, data=body)
+	return json.loads(r.text)["session_token"]
+
+def get_cookie(session_token, userLang, ver):
+	'''Returns a new cookie provided the session_token.'''
+
+	nsoapp_version = get_nsoapp_version()
+
+	global version
+	version = ver
+
+	# timestamp = time.time_ns() // 1000000
+	# guid = str(uuid.uuid4())
+
+	app_head = {
+		'Host':            'accounts.nintendo.com',
+		'Accept-Encoding': 'gzip',
+		'Content-Type':    'application/json; charset=utf-8',
+		'Accept-Language': userLang,
+		'Content-Length':  '439',
+		'Accept':          'application/json',
+		'Connection':      'Keep-Alive',
+		'User-Agent':      'OnlineLounge/' + nsoapp_version + ' NASDKAPI Android'
+	}
+
+	body = {
+		'client_id':     '71b963c1b7b6d119', # Splatoon 2 service
+		'session_token': session_token,
+		'grant_type':    'urn:ietf:params:oauth:grant-type:jwt-bearer-session-token'
+	}
+
+	url = "https://accounts.nintendo.com/connect/1.0.0/api/token"
+
+	r = requests.post(url, headers=app_head, json=body)
+	id_response = json.loads(r.text)
+
+	# get user info
+	try:
+		app_head = {
+			'User-Agent':      'OnlineLounge/' + nsoapp_version + ' NASDKAPI Android',
+			'Accept-Language': userLang,
+			'Accept':          'application/json',
+			'Authorization':   'Bearer {}'.format(id_response["access_token"]),
+			'Host':            'api.accounts.nintendo.com',
+			'Connection':      'Keep-Alive',
+			'Accept-Encoding': 'gzip'
+		}
+	except:
+		print("Not a valid authorization request. Please delete config.txt and try again.")
+		print("Error from Nintendo (in api/token step):")
+		print(json.dumps(id_response, indent=2))
+		sys.exit(1)
+	url = "https://api.accounts.nintendo.com/2.0.0/users/me"
+
+	r = requests.get(url, headers=app_head)
+	user_info = json.loads(r.text)
+
+	nickname = user_info["nickname"]
+
+	# get access token
+	app_head = {
+		'Host':             'api-lp1.znc.srv.nintendo.net',
+		'Accept-Language':  userLang,
+		'User-Agent':       'com.nintendo.znca/' + nsoapp_version + ' (Android/7.1.2)',
+		'Accept':           'application/json',
+		'X-ProductVersion': nsoapp_version,
+		'Content-Type':     'application/json; charset=utf-8',
+		'Connection':       'Keep-Alive',
+		'Authorization':    'Bearer',
+		# 'Content-Length':   '1036',
+		'X-Platform':       'Android',
+		'Accept-Encoding':  'gzip'
+	}
+
+	body = {}
+	try:
+		idToken = id_response["id_token"]
+		f, uuid, timestamp = call_imink_api(idToken, 1)
+
+		parameter = {
+			'f':          f,
+			'naIdToken':  idToken,
+			'timestamp':  timestamp,
+			'requestId':  uuid,
+			'naCountry':  user_info["country"],
+			'naBirthday': user_info["birthday"],
+			'language':   user_info["language"]
+		}
+	except SystemExit:
+		sys.exit(1)
+	except:
+		print("Error(s) from Nintendo:")
+		print(json.dumps(id_response, indent=2))
+		print(json.dumps(user_info, indent=2))
+		sys.exit(1)
+	body["parameter"] = parameter
+
+	url = "https://api-lp1.znc.srv.nintendo.net/v3/Account/Login"
+
+	r = requests.post(url, headers=app_head, json=body)
+	splatoon_token = json.loads(r.text)
+
+	try:
+		idToken = splatoon_token["result"]["webApiServerCredential"]["accessToken"]
+		f, uuid, timestamp = call_imink_api(idToken, 2)
+	except:
+		print("Error from Nintendo (in Account/Login step):")
+		print(json.dumps(splatoon_token, indent=2))
+		sys.exit(1)
+
+	# get splatoon access token
+	try:
+		app_head = {
+			'Host':             'api-lp1.znc.srv.nintendo.net',
+			'User-Agent':       'com.nintendo.znca/' + nsoapp_version + ' (Android/7.1.2)',
+			'Accept':           'application/json',
+			'X-ProductVersion': nsoapp_version,
+			'Content-Type':     'application/json; charset=utf-8',
+			'Connection':       'Keep-Alive',
+			'Authorization':    'Bearer {}'.format(splatoon_token["result"]["webApiServerCredential"]["accessToken"]),
+			'Content-Length':   '37',
+			'X-Platform':       'Android',
+			'Accept-Encoding':  'gzip'
+		}
+	except:
+		print("Error from Nintendo (in Account/Login step):")
+		print(json.dumps(splatoon_token, indent=2))
+		sys.exit(1)
+
+	body = {}
+	parameter = {
+		'id':                5741031244955648,
+		'f':                 f,
+		'registrationToken': idToken,
+		'timestamp':         timestamp,
+		'requestId':         uuid,
+	}
+	body["parameter"] = parameter
+
+	url = "https://api-lp1.znc.srv.nintendo.net/v2/Game/GetWebServiceToken"
+
+	r = requests.post(url, headers=app_head, json=body)
+	splatoon_access_token = json.loads(r.text)
+
+	# get cookie
+	try:
+		app_head = {
+			'Host':                    'app.splatoon2.nintendo.net',
+			'X-IsAppAnalyticsOptedIn': 'false',
+			'Accept':                  'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+			'Accept-Encoding':         'gzip,deflate',
+			'X-GameWebToken':          splatoon_access_token["result"]["accessToken"],
+			'Accept-Language':         userLang,
+			'X-IsAnalyticsOptedIn':    'false',
+			'Connection':              'keep-alive',
+			'DNT':                     '0',
+			'User-Agent':              'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Mobile Safari/537.36',
+			'X-Requested-With':        'com.nintendo.znca'
+		}
+	except:
+		print("Error from Nintendo (in Game/GetWebServiceToken step):")
+		print(json.dumps(splatoon_access_token, indent=2))
+		sys.exit(1)
+
+	url = "https://app.splatoon2.nintendo.net/?lang={}".format(userLang)
+	r = requests.get(url, headers=app_head)
+	return nickname, r.cookies["iksm_session"]
+
+def call_imink_api(id_token, step):
+	'''Passes in an naIdToken to the imink API and fetches the response (comprised of an f token, UUID, and timestamp).'''
+
+	try:
+		api_head = {
+			'User-Agent':   'splatnet2statink/{}'.format(version),
+			'Content-Type': 'application/json; charset=utf-8'
+		}
+		api_body = {
+			'token':       id_token,
+			'hashMethod':  str(step)
+		}
+		api_response = requests.post("https://api.imink.app/f", data=json.dumps(api_body), headers=api_head)
+		resp = json.loads(api_response.text)
+
+		f = resp["f"]
+		uuid = resp["request_id"]
+		timestamp = resp["timestamp"]
+		return f, uuid, timestamp
+	except:
+		try: # if api_response never gets set
+			if api_response.text:
+				print(u"Error from the imink API:\n{}".format(json.dumps(json.loads(api_response.text), indent=2, ensure_ascii=False)))
+			else:
+				print("Error from the imink API: Error {}.".format(api_response.status_code))
+		except:
+			pass
+		sys.exit(1)
+
+def enter_cookie():
+	'''Prompts the user to enter their iksm_session cookie'''
+
+	new_cookie = input("Go to the page below to find instructions to obtain your iksm_session cookie:\nhttps://github.com/frozenpandaman/splatnet2statink/wiki/mitmproxy-instructions\nEnter it here: ")
+	while len(new_cookie) != 40:
+		new_cookie = input("Cookie is invalid. Please enter it again.\nCookie: ")
+	return new_cookie
--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import json
-from random import randint
 import requests
-
-from splatnet.splatnet2.config import Config
-from splatnet.splatnet2.models import Result, Results, Player
 
 import time
 import test_schedule as test
+import splatnet2statink as sp
+import salmonrun
 
 from discord import *
 import discord.ui as bt
@@ -165,145 +163,11 @@ dico_spe = {
     "Ultra Stamp" : "<:stamp:1009100969948229682>",
 }
 
-# class Splatnet2:
-#     config: Config
 
-#     def __init__(self, config: Config):
-#         self.config = config
-#         self.path = "/api/results"
-#         self.response = None
+data = sp.get_all()
 
-#     def results(self) -> Results:
-#         data = self._call("/api/results")
-#         return Results(**data)
-
-#     def result(self, battle_number: str) -> Result:
-#         data = self._call(f"/api/results/{battle_number}")
-#         return Result(**data)
-
-#     def _call(self, path: str) -> dict:
-#         headers = {
-#             "x-unique-id": "32449507786579989234",
-#             "x-requested-with": "XMLHttpRequest",
-#             "x-timezone-offset": self.config.timezone_offset(),
-#             "Accept-Language": self.config.language(),
-#             "Cookie": f"iksm_session={self.config.iksm_session()}",
-#         }
-#         self.response = requests.get(
-#             f"https://app.splatoon2.nintendo.net{path}", headers=headers
-#         )
-
-#         return json.loads(self.response.text)
-
-#     def get_stuff(self, player):
-#         stuff = dico_stuff[player.player.head_skills.main.name] + " | "
-#         for sub_name in player.player.head_skills.subs:
-#             if sub_name == None:
-#                 stuff = stuff + dico_stuff["uk"]
-#             else:
-#                 stuff = stuff + dico_stuff[sub_name.name]
-        
-#         stuff = stuff + f"\n{dico_stuff[player.player.clothes_skills.main.name]} | "
-#         for sub_name in player.player.clothes_skills.subs:
-#             if sub_name == None:
-#                 stuff = stuff + dico_stuff["uk"]
-#             else:
-#                 stuff = stuff + dico_stuff[sub_name.name]
-        
-#         stuff = stuff + f"\n{dico_stuff[player.player.shoes_skills.main.name]} | "
-#         for sub_name in player.player.shoes_skills.subs:
-#             if sub_name == None:
-#                 stuff = stuff + dico_stuff["uk"]
-#             else:
-#                 stuff = stuff + dico_stuff[sub_name.name]
-
-#         return stuff
-
-#     def get_results(self, result:Result):
-
-#         my_team = []
-#         ennemy_team = []
-
-#         player = (result.player_result)
-#         stuff = self.get_stuff(player)
-#         spe = dico_spe[player.player.weapon.special.name]
-#         message_bien = (f"name : {player.player.nickname} , {player.game_paint_point} turf points , rank :{player.player.player_rank}, {player.player.star_rank}* \nweapon used : {player.player.weapon.name} \nkills {player.kill_count} ; assists {player.assist_count} ; deaths {player.death_count} ; specials {spe} {player.special_count}\n{stuff} \n\n")
-#         my_team.append(message_bien)
-
-#         for player in result.my_team_members:
-#             stuff = self.get_stuff(player)
-#             spe = dico_spe[player.player.weapon.special.name]
-#             message_bien = (f"name : {player.player.nickname} , {player.game_paint_point} turf points , rank :{player.player.player_rank}, {player.player.star_rank}* \nweapon used : {player.player.weapon.name} \nkills {player.kill_count} ; assists {player.assist_count} ; deaths {player.death_count} ; specials {spe} {player.special_count}\n{stuff} \n\n")
-#             my_team.append(message_bien)
-
-#         for player in result.other_team_members:
-#             stuff = self.get_stuff(player)
-#             spe = dico_spe[player.player.weapon.special.name]
-#             a = ((f"name : {player.player.nickname} , {player.game_paint_point} turf points , rank :{player.player.player_rank}, {player.player.star_rank}* \nweapon used : {player.player.weapon.name} \nkills {player.kill_count} ; assists {player.assist_count} ; deaths {player.death_count} ; specials {spe} {player.special_count}\n{stuff} \n\n"))
-#             ennemy_team.append(a)
-            
-#         if result.game_mode.name == "Ranked Battle":
-#             if result.my_team_count > result.other_team_count:
-#                 win_state = "won"
-#             else:
-#                 win_state = "lost"
-
-#             duree = str(result.elapsed_time/60).replace(".",":")
-#             score = f"Score : player's team {result.my_team_count} | {result.other_team_count} ennemy team\nroom power : {result.estimate_x_power} | your X power : {result.x_power} "
-        
-#         elif result.game_mode.name == "Turf War":
-#             if result.my_team_percentage > result.other_team_percentage:
-#                 win_state="won"
-#             else:
-#                 win_state = "lost"
-#             duree = "3:00"
-#             score = f"Score : player's team {result.my_team_percentage}% | {result.other_team_percentage}% ennemy team"
-        
-#         elif result.game_mode.name == "Private Battle":
-#             if result.type == "gachi":
-#                 if result.my_team_count > result.other_team_count:
-#                     win_state = "won"
-#                 else:
-#                     win_state = "lost"
-
-#                 duree = str(result.elapsed_time/60).replace(".",":")
-#                 score = f"Score : player's team {result.my_team_count} | {result.other_team_count} ennemy team\nroom power : {result.estimate_x_power} | your X power : {result.x_power} "
-            
-#             elif result.type == "regular":
-#                 if result.my_team_percentage > result.other_team_percentage:
-#                     win_state="won"
-#                 else:
-#                     win_state = "lost"
-#                 duree = "3:00"
-#                 score = f"Score : player's team {result.my_team_percentage}% | {result.other_team_percentage}% ennemy team"
-
-#         if result.game_mode.name == "League Battle":
-#             if result.my_team_count > result.other_team_count:
-#                 win_state = "won"
-#             else:
-#                 win_state = "lost"
-
-#             duree = str(result.elapsed_time/60).replace(".",":")
-#             score = f"Score : player's team {result.my_team_count} | {result.other_team_count} ennemy team\nroom power : {result.estimate_x_power} | your X power : {result.x_power} "
-        
-
-#         game_stats = (f"your team {win_state} on {result.stage.name} ({result.game_mode.name} mode : {result.rule.name})\n{score} \nmatch date {time.ctime(result.start_time)} ({duree[:4]} minutes)" )
-
-#         return my_team, ennemy_team, game_stats
-
-#     def test_splat(self):
-#         headers = {
-#             "x-unique-id": "32449507786579989234",
-#             "x-requested-with": "XMLHttpRequest",
-#             "x-timezone-offset": self.config.timezone_offset(),
-#             "Accept-Language": self.config.language(),
-#             "Cookie": f"iksm_session={self.config.iksm_session()}",
-#         }
-#         response = requests.get(
-#             f"https://app.splatoon2.nintendo.net{self.path}", headers=headers
-#         )
-
-#         print(response.text)
+for k,v in data.items():
+    print(k,v)
 
 # import sys
 
@@ -317,19 +181,3 @@ dico_spe = {
 # token_run = token_bot.tokens["token_bot_splatnet"]
 
 # bot.run(token_run)
-
-config=Config()
-
-headers = {
-    "x-unique-id": "32449507786579989234",
-    "x-requested-with": "XMLHttpRequest",
-    "x-timezone-offset": config.timezone_offset(),
-    "Accept-Language": config.language(),
-    "Cookie": f"iksm_session={config.iksm_session()}",
-}
-response = requests.get(
-    f"https://app.splatoon2.nintendo.net/api/results", headers=headers
-)
-
-print(response.text)
-
